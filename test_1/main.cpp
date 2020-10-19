fb97248a452f506516cd88608a27c702a36b3a1b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-10-19 16:31:45 +0200

    Premier cours de C++

---
+++ test_1/main.cpp
@@ -0,0 +1,81 @@
+# include <iostream>
+# include <limits>
+# include <cstring>
+
+void copy(int size, char* from, char* to){
+    for(int i = 0; i<size; i++)
+    to[i] = from [i];
+}
+
+// int main(int argc, char* argv[]){
+    // char str [] = "hello"; //(copy.....)
+    // // int size strlen(str);
+    // char tab[size];
+    // copy(size,tab,str); //OUPS!!!
+
+    // std::cout << tab<< '\n';
+    // return 0;
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+int max = 4294967295;
+int plus_un (int a) {
+    return a + 1;
+}
+	
+
+
+
+int main(int argc, char* argv[]){
+    std::cout << "coucou c'est moi Moumou la reine des mouettes" << std::endl;
+//     // std::cout << "coucou c'est moi Moumou la reine des mouettes" << std::endl;
+//     // std::cout << "ff       "<<plus_un(max)<< std::endl;
+//     // std::cout <<"argc =   "<<  argc<< "     "<< argv<< std::endl;
+//     // unsigned long long int a = 0;
+//     // // a = "coucou";
+//     // // std::cout <<a<<"  size :   "<<a-1<<std::endl;
+//     // // std::cout<<std::endl<<true<<false<<std::endl;
+//     // // float b = 3.0; double c = 3.0; long double d= 3.0;
+//     // // std::cout<<sizeof(b)<<std::endl<<sizeof(c)<<std::endl<<sizeof(d)<<std::endl;
+//     // std::cout<< std::numeric_limits<long double>::min()<<std::endl;
+
+
+//      // i est une variable de type entier
+//      int i = 78;
+//      // &i est l'adresse où est stoqué i en mémoire
+//     // on la met dans un objet de nom pi et de type int*
+//     int *pi = &i; 
+//     // On affiche cette adresse. Que constatez vous ?
+//      std::cout << pi << std::endl;
+//     //  int *ppi = &*pi;
+//     //       std::cout << ppi << std::endl;
+//     *pi = 28;
+//     std::cout<<i<<std::endl;
+
+
+//     int* p;
+//     std::cout<<"1p== "<< p<<std::endl;
+//     *p  =10;
+//     std::cout<<"2p == = " << *p<<std::endl;
+
+
+
+
+
+
+    return 0;
+}
