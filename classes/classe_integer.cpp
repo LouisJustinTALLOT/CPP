080f5c7a9581b97fbfdbd6a016e1d1563794846b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-04 21:22:45 +0100

    Classe integer avec des bornes qui marche, problème d'accolades....

--- classes/classe_integer.cpp
+++ classes/classe_integer.cpp
@@ -1,62 +1,65 @@
 #include <iostream>
 #include <stdexcept>
+#include <limits>
 
-/*
+
+
 class Integer {
-
-public:
-
-  Integer (const int& x, const int& aa = -1000, const int& bb = 1000 ) : value(x), a(aa), b(bb){
-    if (x<a or x>b){
-      throw std::runtime_error("valeur de x pas dans les bornes");
-      // std::cout << "erreur" << std::endl;
-    }
+  
+  private:
+        int value;
+        int a;
+        int b;
+        
+  public:
     
-  }
+    Integer (const int& x, const int& aa = std::numeric_limits<int>::min(), const int& bb = std::numeric_limits<int>::max() ) : value(x), a(aa), b(bb){
+      if (x<a or x>b){
+        throw std::runtime_error("valeur de x sort des bornes à l'initialisation");
+        // std::cout << "erreur" << std::endl;
+      }
+    }
 
-  void incr(const int& entier){
-    if (value < b){
-      value += entier;
+    void incr(const int& entier = 1){
+      if (value + entier < b){
+        value += entier;
+      }
+      else{
+        throw std::runtime_error("valeur de x sort des bornes par valeur supérieure");
+      }
     }
-    else{
-      throw std::runtime_error("valeur de x pas dans les bornes");
-    }
-  }
+      
+    void decr (const int& entier = 1) {
+      if (value - entier> a){
+        value -= entier;
+      }
+      else{
+        throw std::runtime_error("valeur de x sort des bornes par valeur inférieure");
+      }
+      }
     
-  void decr (const int& entier) {
-    if (value > a){
-      value -= entier;
-    }
-    else{
-      throw std::runtime_error("valeur de x pas dans les bornes");
-    }
-    }
-  }
 
-  void print () {
-    std::cout << value << std::endl;
-  }
+    void print () {
+      std::cout << this->value << std::endl;
+    }
 
-private:
-    int value ;
-    int a;
-    int b;
+
 
 }; // attention ';' obligatoire en fin de déclaration de classe !
 
 // la class Integer est définie
-*/
+
 int main(){
-  /*
+  
   Integer i(13, 0, 100);
   i.print();
 
-  i.incr(33);
+  i.incr(3);
   i.print();
 
   i.decr();
   i.print();
-  */
+  
   return 0;
 
 }
