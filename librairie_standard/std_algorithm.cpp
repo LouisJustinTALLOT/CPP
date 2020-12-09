6474734d496c47ddc1146f25c2fbf19b17404f38
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 15:19:40 +0100

    notre vecteur de travail

--- librairie_standard/std_algorithm.cpp
+++ librairie_standard/std_algorithm.cpp
@@ -5,9 +5,9 @@
 
 int main(){
 
+    std::vector<int> vec = {2,3,1,4,5,6,7,10};
 
 
-        
     // find
 
     auto x =    std::find(vec.begin(), vec.end(),  3);
@@ -19,4 +19,5 @@ int main(){
         std::cout <<"pas ok" <<std::endl;
     }
 
+    return 0;    
 }
