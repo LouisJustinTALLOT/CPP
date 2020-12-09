eba59d7bc57e8719996a904ae4bc69fb0e5430c4
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 15:19:05 +0100

    find

--- librairie_standard/std_algorithm.cpp
+++ librairie_standard/std_algorithm.cpp
@@ -5,8 +5,18 @@
 
 int main(){
 
-    std::vector<int> vec = {2,3,1,4,5,6,7,10};
 
 
         
+    // find
+
+    auto x =    std::find(vec.begin(), vec.end(),  3);
+
+    if (x != vec.end()){
+        std::cout<<"ok"<<std::endl;
+    }
+    else{
+        std::cout <<"pas ok" <<std::endl;
+    }
+
 }
