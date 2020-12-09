3cd5f3ee9d65792882c5a2cadd5e4a8a1137fe7c
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 15:29:34 +0100

    various comments

--- librairie_standard/std_algorithm.cpp
+++ librairie_standard/std_algorithm.cpp
@@ -5,41 +5,21 @@
 
 int main(){
 
-    std::vector<int> vec = {2,3,1,4,5,6,7,10};
+    std::vector<int> vec = {2,3,1,4,5,6,7,10}; //notre vecteur de travail
 
 
     // find
 
     auto x =    std::find(vec.begin(), vec.end(),  3);
 
-    if (x != vec.end()){
-        std::cout<<"ok"<<std::endl;
+    if (x != vec.end()){  
+        std::cout<<"ok"<<std::endl; // ça a été trouvé
     }
     else{
         std::cout <<"pas ok" <<std::endl;
     }
 
-    // sort
-    for (auto x:vec){
-        std::cout <<x<<std::endl;
-    }
-    std::cout <<"-----------------"<<std::endl;
-
-    std::sort(vec.begin(), vec.end());
-
-    for (auto x:vec){
-        std::cout <<x<<std::endl;
-    }
-   
-    std::cout <<"-----------------"<<std::endl;
-
-    std::sort(vec.begin(), vec.end(), [](const int& a, const int& b){
-        return a>b;
-    });
-
-    for (auto x:vec){
-        std::cout <<x<<std::endl;
-    }
-
     return 0;    
-}
+}    std::sort(vec.begin(), vec.end()); //on trie le vecteur
+        std::cout <<x<<std::endl; // on affiche notre vecteur
+        return a>b; //pour trier dans le sens décroissant
