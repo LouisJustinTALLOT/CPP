50fee306c8b3d5d16f055ce38560c834789f7aa5
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 15:34:01 +0100

    algo

--- librairie_standard/std_algorithm.cpp
+++ librairie_standard/std_algorithm.cpp
@@ -3,6 +3,15 @@
 #include <vector>
 #include <iostream>
 
+
+
+
+
+
+
+
+
+
 int main(){
 
     std::vector<int> vec = {2,3,1,4,5,6,7,10}; //notre vecteur de travail
@@ -19,7 +28,27 @@ int main(){
         std::cout <<"pas ok" <<std::endl;
     }
 
-    return 0;    
-}    std::sort(vec.begin(), vec.end()); //on trie le vecteur
+    // sort
+    for (auto x:vec){
+        std::cout <<x<<std::endl;
+    }
+    std::cout <<"-----------------"<<std::endl;
+
+    std::sort(vec.begin(), vec.end()); //on trie le vecteur
+
+    for (auto x:vec){
         std::cout <<x<<std::endl; // on affiche notre vecteur
+    }
+   
+    std::cout <<"-----------------"<<std::endl;
+
+    std::sort(vec.begin(), vec.end(), [](const int& a, const int& b){
         return a>b; //pour trier dans le sens décroissant
+    });
+
+    for (auto x:vec){
+        std::cout <<x<<std::endl;
+    }
+
+    return 0;    
+}
