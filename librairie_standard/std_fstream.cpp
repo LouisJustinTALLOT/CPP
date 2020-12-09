89cbe8cba8576e68b57cee84b9a161349d4bdd2f
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 16:31:28 +0100

    lecture d'un fichier

--- librairie_standard/std_fstream.cpp
+++ librairie_standard/std_fstream.cpp
@@ -12,6 +12,24 @@ int main(){
     file_out <<"Bonjour tout le monde" <<std::endl;
     file_out.close();
     
+    // Lecture
+
+    std::ifstream file_in;
+
+    file_in.open("output.txt");
+    std::string a; 
+    file_in >> a;
+    std::cout<<a<<std::endl; // pb on ne lit que le premmier mot...
+    file_in >> a;
+    std::cout<<a<<std::endl; // puis le 2e...
+
+
+    std::string b;
+    std::getline(file_in, b);
+    std::cout<<b<<std::endl; 
+
+    std::cout<<file_in.eof()<<std::endl; 
+
 
     return 0;
 }
