efc2cf56af04de98a72975cb43c8f093bd96d0fa
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 15:30:19 +0100

    notre classe Integer

--- librairie_standard/std_container.cpp
+++ librairie_standard/std_container.cpp
@@ -12,4 +12,30 @@
 #include <map>
 
 
+class Integer {
+
+private:
+    int _value;
+    int _min;
+    int _max;
+
+public: 
+    Integer(): _value(0){}
+    Integer(const int& x): _value(x){}
+    Integer(const int& x, const int& a, const int& b): _value(x), _min(a), _max(b){
+    }
+
+    void print() const {
+        std::cout << "Value = " << this->_value << std::endl;
+    }
+
+    static void coucou(){
+        std::cout << "Une methode statique " << std::endl;
+    }
+
+    bool operator<(const Integer& other) const {
+        return this->_value < other._value ;
+    }
+
+};
 }
