89e75498b85cb59cb5ffd72c0d1cc56bca2d1b7f
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-09 16:34:43 +0100

    array et vector

--- librairie_standard/std_container.cpp
+++ librairie_standard/std_container.cpp
@@ -40,5 +40,21 @@ public:
 };
 
 int main(){
+    std::array<int, 10> array1 = {1,2,3,4,5};
+    std::vector<int> vector1 = {1,2,3,4,5};
+    std::vector<int> vector2(10);
+    std::vector<int> vector3(10, 1);
+
+    vector1.push_back(100);
+
+    for (int i=0; i<vector1.size(); i++){
+
+        std::cout<< "vector1["<<i<<"] = "<<vector1[i]<<std::endl;
+    }
+
+    for (auto x:vector3){
+        std::cout<< x<<std::endl;
+
+    }
     return 0;
 }
