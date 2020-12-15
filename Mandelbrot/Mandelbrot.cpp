2d1571a349ab7d49afee7d3ce8969a55ae52a03b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-15 23:04:22 +0100

    Tracé de l'ensemble de Mandelbrot en C++

---
+++ Mandelbrot/Mandelbrot.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math>
+#include <fstream>
+
+
+// Nous allons tracer la fractale de Mandelbrot au format .ppm
+// et le sauvegarder en mode binaire
+
+class Complex{ // de la forme a+bi
+private:
+    double a;
+    double b;
+public:
+    Complex(const double& x, const double& y): a(x), b(y) {}
+
+
+
+};
+
+
+vector<int> getColor(int niter, int itermax ){
+  vector<int> col(3);
+  if ( niter < itermax ){
+    col[0] = 200+(55*niter)/100;
+    col[1] = (230*(100-niter))/100;
+    col[2] = (230*(100-niter))/100 ;
+  }
+  else{
+    col[0] = 0;
+    col[1] = 255;
+    col[2] = 255;
+  }
+  return col;
+}
