4e08343a2a6c3b5d9ab128914c14b0b3777c59bf
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-15 23:55:20 +0100

    savoir si c'est dans la fractale ou non

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -18,11 +18,29 @@
 
 
 
-};
 
+int est_dedans(double& x, double& y){
+    double p = sqrt((x-0.25)*(x-0.25) + y*y);
 
-vector<int> getColor(int niter, int itermax ){
-  vector<int> col(3);
+    if(x < p -2 * p * p + 0.25){
+        return 0;
+    }
+
+    if ((x+1)*(x+1) + y*y < 0.0625){
+        return 0;
+    }
+    std::vector<double> z = {0.,0.};
+    std::vector<double> c = {x, y};
+    
+    for(int k = 0; k<100; k++){
+        z[0] = carre_complexe(z)[0] + c[0];
+        z[1] = carre_complexe(z)[1] + c[1];
+        if(z[0]*z[0] + z[1]*z[1] > 4){
+            return k;
+        }
+    }
+    return 0;
+}
   if ( niter < itermax ){
     col[0] = 200+(55*niter)/100;
     col[1] = (230*(100-niter))/100;
