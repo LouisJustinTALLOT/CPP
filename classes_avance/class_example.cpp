bcf6a5c55e7eb3fc514b1d5203115e11ea7faeaa
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-18 21:02:27 +0100

    include

---
+++ classes_avance/class_example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// class Number{
+// private:
+//     std::string _name // ne peut pas être utilisé dans une classe fille
+
+// protected:
+//     std::string _name2 // privé à la classe mais accessible aux classes filles
+
+// };
