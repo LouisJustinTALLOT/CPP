109312db1669c1b73d93eb1db6a5e48bfa9427aa
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-18 21:03:15 +0100

    class Intstack

--- classes_avance/class_example.cpp
+++ classes_avance/class_example.cpp
@@ -8,3 +8,67 @@
 //     std::string _name2 // privé à la classe mais accessible aux classes filles
 
 // };
+
+class IntStack {
+public:
+  IntStack (int s): size(s), top (0), tab (new int [size]) {
+  }
+  IntStack( const IntStack& other){ // le & est nécessaire pour éviter de s'appeler lui-même
+      std::cout <<"Copy constructor "<<std::endl;
+        this->size = other.size;
+        this->top = other.top;
+        this->tab = new int[size];
+        for(int i=0; i<other.top;i++){
+            this->tab[i] = other.tab[i];
+        }
+  }
+  ~IntStack(){
+        std::cout<<"Dans le destructeur"<<std::endl;
+        this->delete_stack();
+  }
+  void push (int e) {
+    tab[top] = e;
+    top = top+1;
+  }
+  int pop () {
+    top = top-1;
+    return tab[top];
+  }
+  void print () {
+    std :: cout << "[";
+    for (int i=0; i<top; i++) {
+      std::cout << tab[i] << ' ';
+    }
+    std::cout << '[' << std::endl;
+  }
+  void address(){
+      std::cout<<this->tab<<std::endl;
+
+  }
+  void delete_stack () {
+    delete [] tab;
+  }
+private:
+  int size;
+  int top;
+  int* tab;
+};
+
+// class Integer: public Number{
+class Integer{
+
+    protected:
+        int _value;
+
+
+    public:
+        Integer(): _value(0){}
+        Integer(const int& x): _value(x){}
+
+        void print(){
+            std::cout <<"Integer with value: " << this-> _value <<std::endl;
+        }
+
+
+};
+
