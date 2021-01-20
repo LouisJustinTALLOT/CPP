225b771d32fb9c1b93e323518b9ccc5d6e28708d
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-01-20 14:24:06 +0100

    template avec vector

--- class_template/template.cpp
+++ class_template/template.cpp
@@ -1,17 +1,34 @@
 #include<iostream>
+#include<vector>
+
+// template<typename T>
+// T add(const T& a, const T& b){
+//     return a+b;
+// }
 
 template<typename T>
-T add(const T& a, const T& b){
-    return a+b;
+void printVector(const std::vector<T> a){
+    for (T x:a){std::cout<<x<<" > ";}
+    std::cout<<std::endl;
 }
 
-int main(){
-    double a, b, c;
-    int d, e, f;
-    a=b = 10.0;
-    d=e = 12;
-    std::cout<<add(a, b)<<std::endl;
-    std::cout<<add(d, e)<<std::endl;
-    // std::cout<<add(a,b)<<std::endl;
 
+int main(){
+    // double a, b, c;
+    // int d, e, f;
+    // a=b = 10.0;
+    // d=e = 12;
+    // std::cout<<add(a, b)<<std::endl;
+    // std::cout<<add(d, e)<<std::endl;
+    // // std::cout<<add(a,b)<<std::endl;
+
+    ///////////
+    std::vector<double> v1={1,2,3,4.4,5};
+    std::vector<int> v2 = {1,2,3,4,5};
+
+    printVector(v1);
+    printVector(v2);
+
+
+    return 0;
 }
