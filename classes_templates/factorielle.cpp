aa4bb24237853822a4c57e4c87d38500f27e552b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-01-20 16:28:54 +0100

    les templates variadiques

---
+++ classes_templates/factorielle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+std::vector<long long int> table_fact = {1};
+
+
+
+long long int factorielle(int n, std::vector<long long int>& table = table_fact){
+
+    
+
+
+}
+
+int main(){
+
+    std::cout <<table_fact[0];
+}
