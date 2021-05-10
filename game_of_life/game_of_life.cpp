778479e3eb321407aef9cdb43242d92c69c1017b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-05-10 21:01:17 +0200

    premier test de compilation

---
+++ game_of_life/game_of_life.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+#define GRID_SIZE 10
+
+// g++ game_of_life.cpp -o game_of_life
+
+// il nous faut une vieille et une nouvelle grille
+
+int main(){
+    std::cout<<GRID_SIZE<<std::endl;
+
+    return 0;
+}
