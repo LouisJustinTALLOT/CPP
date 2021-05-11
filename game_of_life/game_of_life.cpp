cad0f279b6896e15bffeb377d2cac469c3ca1fc5
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-05-11 16:40:35 +0200

    grille rectangulaire

--- game_of_life/game_of_life.cpp
+++ game_of_life/game_of_life.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <array>
-#define GRID_SIZE 10
+
+#define GRID_TYPE std::array<std::array<int, GRID_SIZE_X>, GRID_SIZE_Y>
+#define GRID_SIZE_X 4
+#define GRID_SIZE_Y 2
 
 // g++ game_of_life.cpp -o game_of_life
 
 // il nous faut une vieille et une nouvelle grille
 
 int main(){
-    std::cout<<GRID_SIZE<<std::endl;
+
+    // std::array<std::array<int, GRID_SIZE_X>, GRID_SIZE_Y> main_grid;
+    GRID_TYPE main_grid;
+
+
+    for (int i=0; i<GRID_SIZE_Y; i++){
+        for (int j=0; j<GRID_SIZE_X; j++){
+            // std::cout<< main_grid[j][i]<<"|";
+            std::cout<< 0 <<"|";
+        }
+        std::cout << std::endl;
+    }
+    // std::cout<<main_grid[0][1]<<std::endl;
 
     return 0;
 }
