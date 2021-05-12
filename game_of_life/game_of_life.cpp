efa5c4d5afdd13adb138935f8119a81dd8249593
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-05-12 17:58:50 +0200

    ce qui va arriver à la case en fonction de ses voisines

--- game_of_life/game_of_life.cpp
+++ game_of_life/game_of_life.cpp
@@ -10,6 +10,21 @@
 int old_grid[GRID_SIZE_Y][GRID_SIZE_X];
 int new_grid[GRID_SIZE_Y][GRID_SIZE_X];
 
+int decision(int somme_environnante, int etat_actuel){
+    if (etat_actuel == 0){
+        if (somme_environnante == 3){
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        if (somme_environnante <2 or somme_environnante >3){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
 
 // il nous faut une vieille et une nouvelle grille
 
